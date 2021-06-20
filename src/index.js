a4362c754a9bd6ecbc8276ca8df5d0951f861ab0
Tarush <tarushmohindru1515@gmail.com>
2021-06-20 13:04:14 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -5,14 +5,6 @@ const bot = new Discord.Client();
 const general = new Discord.WebhookClient(process.env.WEBHOOK_ID,process.env.WEBHOOK_TOKEN);
 bot.login(process.env.TOKEN);
 
-function morningMessage(){
-    let time = new Date();
-    let utc = time.getTime() + (time.getTimezoneOffset() * 60000);
-    let nd = new Date(utc + (3600000*+5.5));
-    let ist = nd.toLocaleTimeString();
-}
-setInterval(morningMessage,1000);
-
 bot.on("ready",()=>{
     console.log(`Logged in as ${bot.user.tag}`)
 });
@@ -29,8 +21,7 @@ bot.on("message",msg=>{
                 msg.reply("Hello!");
             }
             if(data.includes("bc") || data.includes("mc") || data.includes("bsdk") || data.includes("chutiya") || data.includes("chutiye")){
-                const embed = new Discord.MessageEmbed().setTitle("Tu Chutiya").setColor("ff0000").setDescription("Tera baap hu bc");
-                msg.channel.send(embed);
+                msg.react("🖕");
             } 
         }else if(!msg.mentions.has(bot.user)){
             if(data.includes("pubg") || data.includes("pubgm")){
